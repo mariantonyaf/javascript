dda31bae1946294074bb76af0a84e1ce1fd5c556
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-03 14:10:44 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,20 +5,25 @@ function contar(){
     let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else{
         res.innerHTML = `Contando: `
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
+        if(p <= 0){
+            window.alert('Passo inválido! Contando PASSO 1')
+            p = 1
+        }
+
         if(i < f){
             for(let c = i; c <= f; c += p){
-                res.innerHTML += `${c} `
+                res.innerHTML += `${c} | `
             }
         } else{
             for(let c = i; c >= f; c -= p){
-                res.innerHTML += `${c} `
+                res.innerHTML += `${c} | `
             }
         }
         res.innerHTML += '\u{1F3C1}'
