fecefe3961103ca6ebcf9b48e03050127093abf3
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-03 12:42:45 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,11 +10,33 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
         if(fsex[0].checked){
             genero = 'Homem'
+
+            if(idade >= 0 && idade < 10){
+
+            } else if(idade < 21){
+
+            } else if(idade < 50){
+
+            } else{
+
+            }
         } else if(fsex[1].checked){
             genero = 'Mulher'
+            
+            if(idade >= 0 && idade <= 10){
+
+            } else if(idade < 21){
+
+            } else if(idade < 50){
+
+            } else{
+                
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos: ${genero} com ${idade} anos.`
