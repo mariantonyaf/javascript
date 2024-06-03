505f61503e7bdb414d177f01eaf4cb4b9bdeb37d
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-03 14:05:37 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,10 +2,25 @@ function contar(){
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
         window.alert('[ERRO] Faltam dados!')
     } else{
-        window.alert('TUDO OK!')
+        res.innerHTML = `Contando: `
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} `
+            }
+        } else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} `
+            }
+        }
+        res.innerHTML += '\u{1F3C1}'
     }
 }
