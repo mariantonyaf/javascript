486c30d31388f2dd57a1684e6f89e6c4836565ea
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-03 08:52:26 -0300

    Update ex013.js

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -11,6 +11,29 @@ var diaSem = agora.getDay()
     6 = Sábado
 */
 
-console.log(diaSem)
-
-switch
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2:
+        console.log('Terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sábado') 
+        break
+    default:
+        console.log('[ERRO] Dia inválido!')
+        break
+}
