f47b2e8ded18a350703ec813cc3db14727065964
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-03 09:50:20 -0300

    ex

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,6 +2,15 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 15
     msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        img.scr = 'manha.png'
+    } else if(hora >= 12 && hora < 18){
+        img.scr = 'tarde.png'
+    } else{
+        img.scr = 'noite.png'
+    }
 }
