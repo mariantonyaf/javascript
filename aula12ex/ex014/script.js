3e7e32f77791c1e08d46522f8cdec2b15344d811
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-03 09:45:22 -0300

    ex

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,4 +1,7 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
-    var msg = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
 }
