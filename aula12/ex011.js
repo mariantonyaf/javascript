73e1feaf8310bb4da9e62ecbeb69022238477d04
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-03 08:03:02 -0300

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,10 +1,8 @@
-var idade = 22
+var idade = 12
 if(idade < 16){
     console.log('Não vota.')
+} else if(idade < 18){
+    console.log('Voto opcional.')
 } else{
-    if(idade < 18){
-        console.log('Voto opcional.')
-    } else{
-        console.log('Vota.')
-    }
+    console.log('Vota.')
 }
