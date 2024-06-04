d4b541fda59e0bdf8c67f2511964ff6916ba6008
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-04 08:22:29 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -3,8 +3,10 @@ let num = [5,9,7,2]
 //num[4] = 6
 //num.push(7)
 //num.length
-//num.sort()
+num.sort()
 
 console.log(num)
-console.log(`O primeiro valor do vetor é ${num[0]}`)
 console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(7)
+console.log(`O valor 7 está na posição ${pos}.`)
