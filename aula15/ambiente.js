fa3fd85d13b340f99f3b0d6e18671aee5a4a81ed
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-04 08:24:05 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -8,7 +8,7 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(7)
+let pos = num.indexOf(10)
 if(pos == -1){
     console.log(`O valor não foi encontrado.`)
 } else{
