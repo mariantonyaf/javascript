0fd1c9ec8d09d403b820bbbadd3439e5bc20c0f6
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-04 07:44:59 -0300

    ex

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -7,9 +7,10 @@ function tabuada(){
     } else{
         let n = Number(num.value)
         let c = 1
+        tab.innerHTML = ''
         while(c <= 10){
             let item = document.createElement('option')
-            item.text = `${num} x ${c} = ${num*c}`
+            item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
             c++
         }
