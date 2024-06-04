e0c0141395e6c1072b7654368898cda2f32932da
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-04 08:18:09 -0300

    Create vetonatela.js

---
+++ aula15/vetonatela.js
@@ -0,0 +1,13 @@
+/*
+let valores = [5, 6, 4, 1, 9, 8]
+
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}.`)
+}
+*/
+
+let valores = [7, 9, 6, 8, 1, 3]
+
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}.`)
+}
