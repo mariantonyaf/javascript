4767c72a5ed814c3603f8a38289465251251b756
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-04 08:18:55 -0300

    Update vetonatela.js

--- aula15/vetonatela.js
+++ aula15/vetonatela.js
@@ -7,6 +7,7 @@ for(let pos = 0; pos < valores.length; pos++){
 */
 
 let valores = [7, 9, 6, 8, 1, 3]
+valores.sort()
 
 for(let pos in valores){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}.`)
