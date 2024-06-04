4cb5c85874757f2f18a31b41774d2347f0604085
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-04 08:23:33 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -9,4 +9,8 @@ console.log(num)
 console.log(`O vetor tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 let pos = num.indexOf(7)
-console.log(`O valor 7 está na posição ${pos}.`)
+if(pos == -1){
+    console.log(`O valor não foi encontrado.`)
+} else{
+    console.log(`O valor 7 está na posição ${pos}.`)
+}
